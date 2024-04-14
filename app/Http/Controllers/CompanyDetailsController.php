b3a599daae3db4d8231e6580090b268c266faa64
iraklikeche <irakli.kechee@gmail.com>
2024-04-14 16:59:28 +0400

    refactor:footer-api

--- app/Http/Controllers/CompanyDetailsController.php
+++ app/Http/Controllers/CompanyDetailsController.php
@@ -2,20 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\CompanyDetail;
 use Illuminate\Http\Request;
 
 class CompanyDetailsController extends Controller
 {
     public function show()
     {
-        $details = [
-            'email' => 'quizwiz@gmail.com',
-            'number' => '+995 328989',
-            'facebook_link' => 'https://facebook.com/user',
-            'linkedIn' => 'https://linkedin.com/in/user'
-        ];
+        $companyDetail = CompanyDetail::firstOrFail();
 
-        return response()->json($details);
+        return response()->json([
+            'email' => $companyDetail->email,
+            'number' => $companyDetail->number,
+            'facebook_link' => $companyDetail->facebook_link,
+            'linkedIn' => $companyDetail->linkedin,
+        ]);
     }
 
 }
